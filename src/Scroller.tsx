7e670b2118519d5b5958cf8d00c8774181c143e1
Faktor 10 <oreillyross@gmail.com>
2020-12-08 14:38:27 +0100

    Fixed typing issue Fixed typing issue

--- src/Scroller.tsx
+++ src/Scroller.tsx
@@ -1,4 +1,7 @@
+//https://github.com/bvaughn/react-window
+
 import * as React from "react";
+import { CSSProperties } from "react";
 import styled from "styled-components";
 import { FixedSizeList as List } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
@@ -6,10 +9,14 @@ import AutoSizer from "react-virtualized-auto-sizer";
 const StyledSection = styled.section`
   width: 100vw;
   height: 100vh;
-  background: grey;
+  background: #c0c0c0;
 `;
 
-const Row = ({ index, style }) => <div style={style}>Row {index}</div>;
+const Row = ({ index, style }: { index: number; style: CSSProperties }) => (
+  <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+    Row {index}
+  </div>
+);
 
 const Scroller = () => {
   return (
